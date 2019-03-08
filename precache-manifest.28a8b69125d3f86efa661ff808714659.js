4fa2779de44a103542696378c3525c55e4593218
Marco Fracassi <fracassi.marco@gmail.com>
2019-03-08 14:10:29 +0100

    Updates

---
+++ precache-manifest.28a8b69125d3f86efa661ff808714659.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cccd0095f9224a0ad066",
+    "url": "/gamification-site/static/css/main.145cd79f.chunk.css"
+  },
+  {
+    "revision": "cccd0095f9224a0ad066",
+    "url": "/gamification-site/static/js/main.34e97ad0.chunk.js"
+  },
+  {
+    "revision": "b962733069787b91ecc5",
+    "url": "/gamification-site/static/js/runtime~main.c5ab8a10.js"
+  },
+  {
+    "revision": "897f3fc96a719285d0ea",
+    "url": "/gamification-site/static/js/2.7d8a71cc.chunk.js"
+  },
+  {
+    "revision": "608381e014f043043030a6baf17da350",
+    "url": "/gamification-site/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gamification-site/precache-manifest.7bd6a21cf22821fd6b8a7b34ce90b957.js"
+  "/gamification-site/precache-manifest.28a8b69125d3f86efa661ff808714659.js"
 );
 
 workbox.clientsClaim();
