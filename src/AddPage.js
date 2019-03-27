ecaf254eccb77dcd160cc6e0bf84146be8f8174e
Marco Fracassi <fracassi.marco@gmail.com>
2019-03-27 14:28:14 +0100

    update multiple links

--- src/AddPage.js
+++ src/AddPage.js
@@ -6,7 +6,29 @@ class ListPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            links: []
+            links: [{url: "http://an.url", type: "submission"}]
+        }
+    }
+
+    addLink() {
+        this.setState((prevState) => ({
+            links: [...prevState.links, {url:"", type:""}],
+        }))
+    }
+
+    updateLink(id, type) {
+        return (event) => {
+            debugger
+            this.setState({
+                links: this.state.links.map((link, index) => {
+                    if (index === id) {
+                        console.log(event.target)
+                        debugger
+                        link[type] = event.target.value
+                    }
+                    return link
+                })
+            })
         }
     }
 
@@ -24,6 +46,9 @@ class ListPage extends Component {
         axios.post(`https://539bk9ow41.execute-api.us-east-1.amazonaws.com/dev/save`, this.state)
             .then(console.log)
             .catch(console.log)
+
+        //TODO: add element in general state list
+        //redirect to list
     }
     
     render() {
@@ -47,10 +72,37 @@ class ListPage extends Component {
                         <label htmlFor="title">Title</label>
                         <input type="text" onChange={this.change("title")} className="form-control" placeholder="Title" />
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="links">Links</label>
-                        <input type="text" onChange={this.change("links")} className="form-control" placeholder="Links" />
-                    </div>
+                    <button onClick={this.addLink.bind(this)}>Add Link</button>
+                    {this.state.links.map((link, idx) => {
+                        let typeId = `type-${idx}`
+                        let urlId = `url-${idx}`
+
+                        return (
+                            <div key={idx}>
+                                <label htmlFor={typeId}>Type</label>
+                                <input
+                                    type="text"
+                                    name={typeId}
+                                    data-id={idx}
+                                    id={typeId}
+                                    className="type"
+                                    value={this.state.links[idx].type}
+                                    onChange={this.updateLink(idx, "type").bind(this)}
+                                />
+                                <label htmlFor={urlId}>Url</label>
+                                <input
+                                type="text"
+                                name={urlId}
+                                data-id={idx}
+                                id={urlId}
+                                className="url"
+                                value={this.state.links[idx].url}
+                                onChange={this.updateLink(idx, 'url').bind(this)}
+                                />
+                            </div>
+                        )
+                    })}
+                    
                     <button type="submit" className="btn btn-primary">Save</button>
                 </form>
             </div>
