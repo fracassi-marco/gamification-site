bced18c62f7c4b1bd0ba0c83db9622425f1e8daa
Alessio Coser <alessio.coser@xpeppers.com>
2019-04-17 14:15:01 +0200

    Updates

---
+++ precache-manifest.94fab53fc9404e14f48d4899d80ed1b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65f0646de5d1380c5893",
+    "url": "/static/css/main.66a65e76.chunk.css"
+  },
+  {
+    "revision": "65f0646de5d1380c5893",
+    "url": "/static/js/main.b815c0f8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4a4b80d0eb3bcdbfebd4",
+    "url": "/static/js/2.574061ee.chunk.js"
+  },
+  {
+    "revision": "49c84c189169a514079a4d6409fdb63f",
+    "url": "/index.html"
+  }
+];
